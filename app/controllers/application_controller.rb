1ae61076bd05d112016ee23fec2340694e629f39
Takroid9 <nt8035294@gmail.com>
2022-02-20 13:01:23 +0000

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+ def set_current_user
+  @current_user = Customer.find_by(id: session[:customer.id])
+ end
+
+ def autheniticate_user
+  if @current_user == nil
+   redirect_to(new_customer_session_path)
+  end
+ end
+
+  def after_sign_in_path_for(resource)
+    public_customers_my_page_path # ログイン後に遷移するpathを設定
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path # ログアウト後に遷移するpathを設定
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
+
+  namespace :public do
+    get 'address/index'
+    get 'address/edit'
+  end
+
+  resources :customers, only: [:show, :edit, :update]
+
+  devise_for :admins, controllers: {
+    registrations: 'admins/registrations'
+  }
+
+
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    registrations: 'customers/registrations'
+  }
+
+  namespace :public do
+    get 'customers/my_page' => 'customers#show'
+    get 'customers/edit'
+    get 'customers/unsubscribe'
+  end
+
+
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#show'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
